93538dcf443afc976b63ca293dcc43124924006e
Dennis <dennispi@gmail.com>
2016-06-24 15:03:17 -0700

    Initial Commit

---
+++ Taco Finder/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Taco Finder
+//
+//  Created by Dennis Pilarinos on 2016-06-24.
+//  Copyright © 2016 Dennis Pilarinos. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
